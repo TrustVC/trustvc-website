127bc740ee7fddea9b33e3f7de3359703cecbc2b
Rishabh Singh <rishabh7singh5@gmail.com>
2026-04-20 14:49:23 +0530

    fix: align center

--- src/components/AssetManagementPanel/AssetManagementForm/FormVariants/ActionSelectionForm/ActionSelectionForm.tsx
+++ src/components/AssetManagementPanel/AssetManagementForm/FormVariants/ActionSelectionForm/ActionSelectionForm.tsx
@@ -114,12 +114,12 @@ export const ActionSelectionForm: FunctionComponent<
           <div className="vr-title-col" />
         </div>
       )}
-      <div className="vr-footer">
+      <div className="vr-footer flex-col gap-2 md:flex-row md:gap-0">
         {(isReturnedToIssuer || isTokenBurnt) && (
-          <div className="flex justify-start w-full">
+          <div className="flex justify-center md:justify-start w-full max-w-[638px] ">
             <Tag
               rounded="rounded-full"
-              className="bg-[#FDDAE2] !p-2 min-w-[188px] max-w-[383px] text-center w-full"
+              className="bg-[#FDDAE2] !p-2 min-w-[188px] max-w-[383px] text-center w-full flex-1"
             >
               <h4 className="bg-alert-20">
                 {isReturnedToIssuer
@@ -127,13 +127,14 @@ export const ActionSelectionForm: FunctionComponent<
                   : 'ETR Taken Out of Circulation'}
               </h4>
             </Tag>
-            <div className="vr-footer-dropdown-placeholder" />
+            <div className="vr-footer-dropdown-placeholder hidden lg:block flex-1" />
           </div>
         )}
         {!isTokenBurnt && (
-          <div className="dropdown-btn-frame ">
-            <div className="vr-footer-dropdown-placeholder" />
+          <div className="dropdown-btn-frame flex gap-2">
+            <div className="vr-footer-dropdown-placeholder hidden lg:block flex-1" />
 
+            {/* <div className="flex-1"> */}
             {account ? (
               <>
                 {canManage ? (
@@ -173,6 +174,7 @@ export const ActionSelectionForm: FunctionComponent<
               </Button>
             )}
           </div>
+          // </div>
         )}
       </div>
     </>
