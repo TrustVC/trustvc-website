d63b4ece9ed983e2758360de53910b9244366e26
manishdex25 <manish.singh@dextech.ai>
2026-04-24 14:40:04 +0530

    refactor: update environment configuration

--- src/configs/chain-config.ts
+++ src/configs/chain-config.ts
@@ -1,24 +1,7 @@
 import { CHAIN_ID } from '@trustvc/trustvc'
 
 // Re-export environment config
-export {
-  IS_DEVELOPMENT,
-  STABILITY_API_KEY,
-  STABILITY_TESTNET_API_KEY,
-  ASTRON_TESTNET_API_KEY,
-  INFURA_API_KEY,
-  ALCHEMY_API_KEY,
-  ETHERSCAN_BASE_URL,
-  GA_MEASUREMENT_ID,
-  GA_CONFIG_OPTION,
-  IS_MAINNET,
-  MAGIC_API_KEY,
-  NETWORK,
-  NETWORK_NAME,
-  NETWORK_ID,
-  IS_TEST_ENV,
-  IS_DEV_SERVER,
-} from './env-config'
+export { NETWORK_NAME, IS_TESTNET, INFURA_API_KEY } from './env-config'
 
 /**
  * Supported networks in production environment
