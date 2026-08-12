80f44263cfcdc4826757507da8dab27458130c26
manishdex25 <manish.singh@dextech.ai>
2026-08-12 16:21:51 +0530

    feat: update links

--- src/components/home/VerifySection/VerifyError.tsx
+++ src/components/home/VerifySection/VerifyError.tsx
@@ -92,7 +92,7 @@ const VerifyError: React.FC<VerifyErrorProps> = ({
               {/* What Should I Do? link */}
               <div className="flex-1 max-w-[360px] min-w-[240px] p-1 flex flex-col justify-center items-center gap-2.5">
                 <a
-                  href="https://docs.tradetrust.io"
+                  href="https://docs.trustvc.io"
                   target="_blank"
                   rel="noopener noreferrer"
                   data-testid="what-should-i-do-btn"
